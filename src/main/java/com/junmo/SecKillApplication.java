cbe1728093b8ecf0335ed64f8961fd13f2f67aca
suzzt <suzzt@163.com>
2020-05-10 22:10:34 +0800

    ~秒杀开始~

---
+++ src/main/java/com/junmo/SecKillApplication.java
@@ -0,0 +1,13 @@
+package com.junmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@MapperScan(value = "com.junmo.mapper")
+@SpringBootApplication
+public class SecKillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecKillApplication.class, args);
+	}
+}
